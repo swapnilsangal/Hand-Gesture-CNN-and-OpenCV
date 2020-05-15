3d5edf22812e6e7674aba8fa01b9aa9effdbe23f
Swapnil Sangal <35949322+swapnilsangal@users.noreply.github.com>
2020-05-15 18:03:20 +0530

    OpenCV_Camera_Application Update
    Adding the intermediate windows to the vending machine application.

--- OpenCV_Camera_Appication.py
+++ OpenCV_Camera_Appication.py
@@ -9,6 +9,7 @@ import tensorflow as tf
 import numpy as np
 from datetime import datetime
 
+
 #Dictionary for Class Lables to the corresponding output layer for the CNN M1_D3
 class_label_lookup={0:'Zero',1:'One',2:'Two',3:'Three',4:'Four',5:'Five',6:'A',7:'I',8:'Undefined'}
 
@@ -20,35 +21,93 @@ loaded_model = tf.keras.models.load_model('Models/M1_D3')
 input_height = 64
 input_width = 64
 
+#Initialise Camera capture 
 cap = cv2.VideoCapture(0)
 width  = int(cap.get(3))  # float
 height = int(cap.get(4)) # float
+fps = cap.get(cv2.CAP_PROP_FPS)
 
+
+#Initialise the Recognition area
 rect_pt1 = (int(width*0.70),int(height*0.20))
 rect_pt2 = (int(width),int(height*0.80))
 
+
+#Display window Width and Height
+disp_window_width = int(width*0.7)
+disp_window_height = height
+
+#Initialise Frame Count
 count=0
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc,int(fps),(width,height))
 
+
+#Initialise window number
 window_no = 1
 
-last_n = 5
+#Initialise Recognition Frequency after N Frames
+N=6
+
+#Initialise the last N deteions to be tracked
+last_n = 10
 last_n_detections = [np.NaN]*last_n
+
+
+#Load App Pictures
+sayhi = cv2.imread('Images/five-gesture.png',cv2.IMREAD_UNCHANGED)
+bgr = sayhi[:,:,:3]
+sayhi_width,sayhi_height = int(disp_window_width/1.5),int(disp_window_width/5)
+sayhi = cv2.resize(bgr,(sayhi_width,sayhi_height))
+sayhi_height_offset = int(disp_window_height/2)-int(sayhi_height/2)
+sayhi_width_offset = int(disp_window_width/2)-int(sayhi_width/2)
+
+order_menu = cv2.imread('Images/Window2_Order_Menu.png',cv2.IMREAD_UNCHANGED)
+bgr = order_menu[:,:,:3]
+order_menu_width,order_menu_height = int(disp_window_width),int(disp_window_width/1.5)
+order_menu = cv2.resize(bgr,(order_menu_width,order_menu_height))
+order_menu_height_offset = int(disp_window_height/2)-int(order_menu_height/2)
+order_menu_width_offset = int(disp_window_width/2)-int(order_menu_width/2)
+
+
+oops_menu_width,oops_menu_height = int(disp_window_width),int(disp_window_width/1.5)
+
+oops_one = cv2.imread('Images/Oops_one.png',cv2.IMREAD_UNCHANGED)[:,:,:3]
+oops_one = cv2.resize(oops_one,(oops_menu_width,oops_menu_height))
+
+oops_two = cv2.imread('Images/Oops_Two.png',cv2.IMREAD_UNCHANGED)[:,:,:3]
+oops_two = cv2.resize(oops_two,(oops_menu_width,oops_menu_height))
+
+oops_three = cv2.imread('Images/Oops_Three.png',cv2.IMREAD_UNCHANGED)[:,:,:3]
+oops_three = cv2.resize(oops_three,(oops_menu_width,oops_menu_height))
+
+oops_four = cv2.imread('Images/Oops_Four.png',cv2.IMREAD_UNCHANGED)[:,:,:3]
+oops_four = cv2.resize(oops_four,(oops_menu_width,oops_menu_height))
+
+oops_menu_height_offset = int(disp_window_height/2)-int(oops_menu_height/2)
+oops_menu_width_offset = int(disp_window_width/2)-int(oops_menu_width/2)
+
+#Initialise menu frame count and kill time
+oops_count = 0
+order_menu_count = 0
+kill_after_seconds = 5
+kill_after_frame = fps*kill_after_seconds
 
 while(True):
 
     ret, frame = cap.read()
-
+    
     frame = cv2.flip(frame,1)
     
     # Running the Classifier at every n'th' Frame
-    if count%10==0:
+    if count%N==0:
         count=0
         
         #Cropping the Region of Interest to feed to the CNN
         cropped = frame[rect_pt1[1]:rect_pt2[1] , rect_pt1[0]:rect_pt2[0]]
         cropped_gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
+        
         '''
         #Implementing Image Enhancement Techniques here
         kernel = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])       
@@ -77,10 +136,6 @@ while(True):
     k = cv2.waitKey(1)
     if k == ord('q'):
         break
-    elif k==ord('h'):
-        window_no = 2
-    elif k==ord('n'):
-        window_no = 1
     
     #Show the whole frame
     # Blurring the rest of the window
@@ -113,9 +168,42 @@ while(True):
     frame = cv2.putText(frame, now_time_text, org=(width-int(time_textsize[0])-10,10+int(time_textsize[1]/2)),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=0.3,color=(255,0,0))
     frame = cv2.putText(frame, welcome_text, org=(int((width/2)-welcome_textsize[0]/2),10+int(welcome_textsize[1]/2)),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=0.3,color=(255,0,0))
     
+    if window_no == 1:
+        frame[sayhi_height_offset:sayhi_height_offset+sayhi_height,sayhi_width_offset:sayhi_width_offset+sayhi_width] = frame[sayhi_height_offset:sayhi_height_offset+sayhi_height,sayhi_width_offset:sayhi_width_offset+sayhi_width]*0.1+sayhi*0.9
+        if last_n_detections==['Five']*last_n:
+            window_no = 2
+            
+    elif window_no == 2:
+        order_menu_count +=1
+        frame[order_menu_height_offset:order_menu_height_offset+order_menu_height,order_menu_width_offset:order_menu_width_offset+order_menu_width] = frame[order_menu_height_offset:order_menu_height_offset+order_menu_height,order_menu_width_offset:order_menu_width_offset+order_menu_width]*0.1+order_menu*0.9 
+        if last_n_detections==['Zero']*last_n:
+            window_no = 1
+        elif order_menu_count == kill_after_frame:
+            order_menu_count = 0
+            window_no = 1
+        elif last_n_detections in [['One']*last_n,['Two']*last_n,['Three']*last_n,['Four']*last_n]:
+            window_no = 3
+            detected_item = last_n_detections[0]
+            
+    elif window_no == 3:
+        oops_count +=1
+        if detected_item == 'One':
+            oops_pic = oops_one
+        elif detected_item == 'Two':
+            oops_pic = oops_two
+        elif detected_item == 'Three':
+            oops_pic = oops_three
+        elif detected_item == 'Four':
+            oops_pic = oops_three
+            
+        frame[oops_menu_height_offset:oops_menu_height_offset+oops_menu_height,oops_menu_width_offset:oops_menu_width_offset+oops_menu_width] =  frame[oops_menu_height_offset:oops_menu_height_offset+oops_menu_height,oops_menu_width_offset:oops_menu_width_offset+oops_menu_width]*0.1+oops_pic*0.9
+            
+        if oops_count==kill_after_frame:
+            oops_count = 0
+            window_no = 1
     #Show the Frame
     cv2.imshow('Hand Gesture Project',frame)
-    
+    out.write(frame)
     '''
     #Show the cropped and processed frame only
     cropped_gray = cv2.putText(cropped_gray, text, org=(10,20),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=0.5,color=(255,0,0))
@@ -126,4 +214,6 @@ while(True):
 
 # When everything done, release the capture
 cap.release()
+out.release()
 cv2.destroyAllWindows()
+
